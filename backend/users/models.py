c8139b6ec64c71a261666081e637fda8b7b93ec2
SteepDay <steepday@gmail.com>
2025-05-08 15:31:53 +0500

    Auth: добавлена модель User + миграции

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLES = (
+        ('HR', 'HR'),
+        ('MANAGER', 'Manager'),
+    )
+    role = models.CharField(max_length=10, choices=ROLES, default='HR')
